11a9bb8839c24e2ee4273f1cd5b9e9b0f7dc5cea
makarovdv <makar-hxc@mail.ru>
2017-11-19 22:40:03 +0600

    3 15 SQL scripts

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,8 +1,11 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
+DROP SEQUENCE IF EXISTS global_seq_meal;
 
 CREATE SEQUENCE global_seq START 100000;
+CREATE SEQUENCE global_seq_meal START 1;
 
 CREATE TABLE users
 (
@@ -21,5 +24,17 @@ CREATE TABLE user_roles
   user_id INTEGER NOT NULL,
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE meals
+(
+  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq_meal'),
+  user_id          INTEGER                 NOT NULL,
+  date_time        TIMESTAMP               NOT NULL,
+  description      VARCHAR                 NOT NULL,
+  calories         INTEGER                 NOT NULL,
+--   PRIMARY KEY (id),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
