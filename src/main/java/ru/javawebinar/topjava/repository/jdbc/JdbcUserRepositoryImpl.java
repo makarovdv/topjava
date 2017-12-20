59e34d49ea9584b508f157c90b5d19dc89d9b896
makarovdv <makar-hxc@mail.ru>
2017-12-20 06:12:24 +0600

    6 25 reformat JdbcUserRepositoryImpl

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
@@ -1,12 +1,15 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.support.DataAccessUtils;
-import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Role;
@@ -14,23 +17,42 @@ import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
 import javax.sql.DataSource;
-import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
 
 @Transactional(readOnly = true)
 @Repository
 public class JdbcUserRepositoryImpl implements UserRepository {
+    private static final BeanPropertyRowMapper<User> ROW_MAPPER = BeanPropertyRowMapper.newInstance(User.class);
 
-    private static final UserWithRolesExtractor extractor = new UserWithRolesExtractor();
+    private static final ResultSetExtractor<List<User>> extractor = new ResultSetExtractor<List<User>>() {
+        @Nullable
+        @Override
+        public List<User> extractData(ResultSet rs) throws SQLException, DataAccessException {
+            Map<Integer, User> users = new LinkedHashMap<>();
+            User user;
+            while (rs.next()){
+                Integer id = rs.getInt("id");
+                user = users.get(id);
+                if (user==null){
+                    user = ROW_MAPPER.mapRow(rs,rs.getRow());
+                    user.setRoles(EnumSet.noneOf(Role.class));
+                    users.put(id,user);
+                }
+                HashSet<Role> roles = new HashSet<>(user.getRoles());
+                roles.add(Role.valueOf(rs.getString("role")));
+                user.setRoles(EnumSet.copyOf(roles));
+            }
+            return new ArrayList<>(users.values());
+        }
+    };
 
     private final JdbcTemplate jdbcTemplate;
 
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     private final SimpleJdbcInsert insertUser;
-
-    private final UsersComparator comparator = new UsersComparator();
 
     @Autowired
     public JdbcUserRepositoryImpl(DataSource dataSource, JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
@@ -53,7 +75,7 @@ public class JdbcUserRepositoryImpl implements UserRepository {
             insertRoles(user);
         } else {
             User inBase = get(user.getId());
-            if(comparator.compare(inBase,user)!=0) {
+            if (!equalsIgnoreRolesAndId(inBase,user)) {
                 int rows = namedParameterJdbcTemplate.update("" +
                         "UPDATE users SET name=:name, email=:email, password=:password, " +
                         "registered=:registered, enabled=:enabled, calories_per_day=:caloriesPerDay " +
@@ -62,8 +84,7 @@ public class JdbcUserRepositoryImpl implements UserRepository {
                     return null;
                 }
             }
-            if(!user.getRoles().containsAll(inBase.getRoles())||
-                    !inBase.getRoles().containsAll(user.getRoles())){
+            if (!user.getRoles().equals(inBase.getRoles())){
                 deleteRoles(user);
                 insertRoles(user);
             }
@@ -100,31 +121,28 @@ public class JdbcUserRepositoryImpl implements UserRepository {
     @Override
     public List<User> getAll() {
         String sql = "SELECT * FROM users u " +
-                "left join user_roles r on u.id = r.user_id";
+                "     left join user_roles r on u.id = r.user_id " +
+                "     ORDER BY u.name, u.email";
         return namedParameterJdbcTemplate.query(sql, extractor);
     }
 
     private void deleteRoles(User user){
-        Integer id = user.getId();
-        jdbcTemplate.update("DELETE FROM user_roles WHERE user_id=?", id);
+        jdbcTemplate.update("DELETE FROM user_roles WHERE user_id=?", user.getId());
     }
 
     private void insertRoles(User user){
-        final List<Role> roles = new ArrayList<>(user.getRoles());
-        Integer id = user.getId();
-        String sql = "INSERT INTO user_roles (role, user_id) VALUES (?, ?)";
-        jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
-            @Override
-            public void setValues(PreparedStatement ps, int i) throws SQLException {
-                Role role = roles.get(i);
-                ps.setString(1, role.toString());
-                ps.setInt(2,id);
-            }
+        List<Object[]> batchArgs = new ArrayList<>();
+        new ArrayList<>(user.getRoles())
+                .forEach(r -> batchArgs.add(new Object[]{r.toString(),user.getId()}));
+        jdbcTemplate.batchUpdate("INSERT INTO user_roles (role, user_id) VALUES (?, ?)",batchArgs);
+    }
 
-            @Override
-            public int getBatchSize() {
-                return roles.size();
-            }
-        });
+    private boolean equalsIgnoreRolesAndId(User u1, User u2){
+        if (!u1.getName().equals(u2.getName())) return false;
+        if (!u1.getEmail().equals(u2.getEmail())) return false;
+        if (!u1.getRegistered().equals(u2.getRegistered())) return false;
+        if (u1.isEnabled() != u2.isEnabled()) return false;
+        if (u1.getCaloriesPerDay() != u2.getCaloriesPerDay()) return false;
+        return u1.getPassword().equals(u2.getPassword());
     }
 }
